53d301a220d9442396b98975611474b4c84357b7
arllyepc <arllyepc@gmail.com>
2024-01-26 21:41:18 -0300

    register function

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,13 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
+
+import {db} from "@/lib/db";
 
 import { RegisterSchema } from "@/schemas";
+
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -13,7 +18,29 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     };
   }
 
+  const { email, name, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const exitingUser = await getUserByEmail(email);
+  
+  if (exitingUser) {
+    return {
+      error: "Email already in use",
+    };
+  }
+
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: SEND VERIFICATION TOKEN EMAIL
+
   return {
-    success: "Email sent! ",
+    success: "User created! ",
   };
 };
